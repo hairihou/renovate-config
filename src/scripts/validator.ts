6f891fa99e8430bfdbf1831cdd16f3f50ee741b4
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2024-09-19 10:18:52 +0900

    chore(): template

---
+++ src/scripts/validator.ts
@@ -0,0 +1,29 @@
+import { execSync } from 'node:child_process';
+import { readdirSync } from 'node:fs';
+
+const ignoreTargetFileNames: string[] = ['biome.json', 'package.json', 'tsconfig.json'] as const satisfies string[];
+
+const fileIsIgnoreTarget = (file: string): boolean =>
+  !(file.endsWith('.json') || file.endsWith('.json5')) || ignoreTargetFileNames.includes(file);
+
+const validateConfig = (): void => {
+  const files: string[] = readdirSync('.').filter((file) => !fileIsIgnoreTarget(file));
+  const errorFiles: string[] = [];
+
+  for (const file of files) {
+    try {
+      const result = execSync(`npx --yes --package renovate -- renovate-config-validator --strict ${file}`);
+      console.info(result.toString());
+    } catch (error) {
+      // biome-ignore lint/suspicious/noExplicitAny: renovate error output is not typed
+      console.error((error as any).output.toString());
+      errorFiles.push(file);
+    }
+  }
+
+  if (errorFiles.length > 0) {
+    process.exit(1);
+  }
+};
+
+validateConfig();
